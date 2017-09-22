11b7d09c2b48de560826ad0c62aa1ddb31c074b7
Raymond Tukpe <rtukpe@gmail.com>
2017-09-22 03:05:34 +0100

    1. Added /events PUT api

--- server/index.js
+++ server/index.js
@@ -16,8 +16,11 @@ app.listen(3001);
 app.use(bodyParser.json({ type: "application/json" }));
 app.use(cors());
 
+
+/**
+ * @method GET /events
+ */
 app.get("/events", function (r, s) {
-	console.log("stuff");
 	Event.find()
 		.then(events => {
 			return s.json({
@@ -94,17 +97,40 @@ app.post("/events", function (r, s) {
  * @param {Number} price
  * @param {Date} startDate
  * @param {Date} endDate
+ * @example 
+ * ```
+ * {
+	"_id": "59c43574bd9cdc72eee95da3",
+	"update": {
+		  "title": "Xtreme Paintball 3",
+	      "location": "Mainland Sports, Junction, Ikeja.",
+	      "price": 1000,
+	      "description":"Loipisicing elit. Quia veritatis laboriosam sed ullam consequatur non reiciendis repellendu , maxime libero dolorem itaque.",
+	      "startTime": "7pm",
+	      "endTime":"11pm"
+	}
+}
+ * ```
  */
 app.put('/events', (r, s) => {
 	Event.update({ _id: r.body._id }, r.body.update)
 		.then(event => {
-			return s.json({ status: "success", data: event });
+			Event.findById(r.body._id)
+				.then(event => {
+					return s.json({ status: "success", data: event });
+				})
+				.catch(err => {
+					return s.json({ status: "failed", err: err });
+				});
 		})
 		.catch(err => {
 			return s.json({ status: "failed", err: err });
 		});
 })
 
+/**
+ * @method GET /event:id
+ */
 app.get("/event/:id", function (req, res) {
 	Event.findOne({ _id: req.params.id }, function (err, event) {
 		res.send(event)
@@ -122,6 +148,6 @@ app.post("/auth", function (req, res) {
 const getDaysFromEvents = events => {
 	return events
 		.map(event => moment(event.date).format("YYYYMMDD"))
-		.filter((elem, pos, arr) =>  arr.indexOf(elem) === pos)
+		.filter((elem, pos, arr) => arr.indexOf(elem) === pos)
 		.sort();
 };
