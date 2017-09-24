f71599cf24f94571b8dcbd4578f74cb93c4f6a50
Onyekachi Mbaike <onyekachi.mbaike@gmail.com>
2017-09-24 13:11:58 +0100

    Put all reducers in one file

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,14 +1,49 @@
 import { combineReducers } from "redux";
 import { routerReducer } from "react-router-redux";
 
-import activeDate from "./activeDate";
-import days from "./days";
-import events from "./events";
+function activeDate(state = "", action) {
+  switch (action.type) {
+    case "SET_ACTIVE_DATE":
+      return action.payload;
+    case "EVENTS_FETCH_SUCCEEDED":
+      return action.payload.days[0] || state;
+    default:
+      return state;
+  }
+}
+
+function days(state = [], action) {
+  switch (action.type) {
+    case "EVENTS_FETCH_SUCCEEDED":
+      return action.payload.days;
+    default:
+      return state;
+  }
+}
+
+function events(state = [], action) {
+  switch (action.type) {
+    case "EVENTS_FETCH_SUCCEEDED":
+      return action.payload.events;
+    default:
+      return state;
+  }
+}
+
+function loading(state = false, action) {
+  return action.type === "EVENTS_FETCH_REQUESTED" || false;
+}
+
+function error(state = false, action) {
+  return action.type === "EVENTS_FETCH_FAILED" || false;
+}
 
 const rootReducer = combineReducers({
   activeDate,
   days,
   events,
+  error,
+  loading,
   router: routerReducer
 });
 
